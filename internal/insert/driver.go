368de88c484fa9b90dbda271f474d8d1bb245e38
Tetsuro Mikami <tetsuro@mickamy.com>
2026-05-19 22:55:17 +0900

    feat(insert): add MySQL driver

--- internal/insert/driver.go
+++ internal/insert/driver.go
@@ -16,11 +16,15 @@ type Driver interface {
 
 func openDriver(ctx context.Context, dataSourceName string) (Driver, error) {
 	switch dsn.Scheme(dataSourceName) {
+	case "mysql":
+		return newMySQLDriver(ctx, dataSourceName)
 	case "postgres", "postgresql":
 		return newPostgresDriver(ctx, dataSourceName)
 	case "":
 		return nil, dsn.ErrMissingScheme
 	default:
-		return nil, fmt.Errorf("unsupported DSN scheme %q; supported: postgres, postgresql", dsn.Scheme(dataSourceName))
+		scheme := dsn.Scheme(dataSourceName)
+
+		return nil, fmt.Errorf("unsupported DSN scheme %q (supported: mysql, postgres)", scheme)
 	}
 }
